7f650441efbc2f4c912a13d027641eb6ad7bc6e3
romoguill <romoguill@gmail.com>
2024-05-30 18:21:50 -0300

    Updated location for nextjs project

--- apps/client/jest.config.ts
+++ apps/client/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'agro-management_v2',
+  displayName: 'client',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/agro-management_v2',
+  coverageDirectory: '../../coverage/apps/client',
 };
